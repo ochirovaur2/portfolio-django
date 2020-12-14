ee4c370d253028f1c15cfe9ee59ebc5ea42eb6a6
Ochirov Aiur <ochirovaur2@mail.ru>
2020-12-15 00:10:42 +0700

    Update wsgi.py

--- portfolio/portfolio/wsgi.py
+++ portfolio/portfolio/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
 
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
