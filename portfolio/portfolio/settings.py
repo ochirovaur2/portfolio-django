89d4ccbff1e6a68411e7ba42f9050bf2debff3c8
Ochirov Aiur <ochirovaur2@mail.ru>
2020-12-16 08:50:35 +0700

    Update settings.py

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -118,5 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_URL = '/home/aiur/projects/portfolio/portfolio-django/portfolio/portfolio/static/' 
-# STATIC_URL = '/static/'
+# STATIC_URL = '/home/aiur/projects/portfolio/portfolio-django/portfolio/portfolio/static/' 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
